9cb08abbd899c6e23f62f77e59f52e19116923f4
Salsa177 <salvatoregtumminia@gmail.com>
2025-01-14 20:54:09 +1100

    stuf

--- PYTHON/WEBSITE/auth.py
+++ PYTHON/WEBSITE/auth.py
@@ -59,9 +59,10 @@ def sign_up():
             new_user = User(email=email, username=username, password=generate_password_hash(password, method='pbkdf2:sha256'))
             db.session.add(new_user)
             db.session.commit()
-            login_user(user, remember=True)
+            logged_in = True
             flash('Account created succesfully', category='success')
-            return redirect(url_for('views.home'))
+            return redirect(url_for('auth.login'))
+            
 
     return render_template("signup.html")
     
